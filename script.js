ebf12afa41cd3243cca27cbcad8656085b5f7fe1
James <jamesnorbertowork@gmail.com>
2021-01-06 09:37:24 +0900

    my first arrow function is working

---
+++ script.js
@@ -0,0 +1,15 @@
+/******************************
+LISTEN FOR KEYPRESSES
+******************************/
+
+// setup an 'event delegation' pattern to listen (since keys are children of ".keyboard")
+const myCalculator = document.querySelector('.calculator'); // return the first elment in the doc with the class ".calculator"
+const myKeyboard = document.querySelector('.keyboard'); // return the first elment in the doc with the class ".keyboard"
+
+// do the listening
+myKeyboard.addEventListener('click', localFunction => {
+    if (localFunction.target.matches('button')) {
+        alert("Your momma");
+    }
+}
+)
